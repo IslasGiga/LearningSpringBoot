40bf59d0ab1edba84b7cedb401451ad496a303ad
Islas Girão Garcia <islasgiga@gmail.com>
2020-02-04 16:38:29 -0300

    Creating new instances operations

--- src/main/java/com/example/learning/LearningApplication.java
+++ src/main/java/com/example/learning/LearningApplication.java
@@ -1,13 +1,33 @@
 package com.example.learning;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class LearningApplication {
+import com.example.learning.domain.Category;
+import com.example.learning.repository.CategoryRepository;
+import com.sun.xml.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
+@SpringBootApplication
+public class LearningApplication implements CommandLineRunner{
+	
+	@Autowired
+	CategoryRepository repo;
 	public static void main(String[] args) {
 		SpringApplication.run(LearningApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+			
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+		
+		repo.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
