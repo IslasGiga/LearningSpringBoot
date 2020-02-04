23a4b9f581cc72f53b011b3364fc6cbd4cc3bcb3
Islas Girão Garcia <islasgiga@gmail.com>
2020-02-04 14:31:20 -0300

    Creating the first domain class

--- src/main/java/com/example/learning/resources/CategoryResource.java
+++ src/main/java/com/example/learning/resources/CategoryResource.java
@@ -1,16 +1,30 @@
 package com.example.learning.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.learning.domain.Category;
 
 @RestController
 @RequestMapping(value="categories")
 public class CategoryResource {
 
 	
+	
 	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "REST Funcionando com sucesso";
+	public List<Category> list() {
+		
+		Category cat1 = new Category(1, "Informática");
+		Category cat2 = new Category(2, "Escritório");
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		
+		return list;
 	}
 }
